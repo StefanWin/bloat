3dd3f5e6c8c732b91799f697ed326acf7fef305e
Stefan Wintergerst <stefan.wintergerst@uni-ulm.de>
2020-12-10 04:56:32 +0100

    fixed formatting

--- include/Shader.h
+++ include/Shader.h
@@ -4,4 +4,4 @@
 
 // Adopted from https://github.com/opengl-tutorials/ogl
 
-GLuint load_shaders(const char * vertex_file_path,const char * fragment_file_path);
+GLuint load_shaders(const char *vertex_file_path, const char *fragment_file_path);
